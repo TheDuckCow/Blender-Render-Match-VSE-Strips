6ef6c004c558342922e3f2c57122521c45513f2b
TheDuckCow <theduckcow@live.com>
2017-04-09 09:59:58 -0400

    added another modifier for showing and hiding modifiers

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Blender Render Match VSE Strips
-This is a one-button addon that does something I do very often: takes the currently active sequence in blender and sets the render to match its resolution and length (length is based on all selected sequences, resolution is based on just the active sequence)
+This operator helps setup something I do very often: it takes the currently active sequence in blender and sets the render to match its resolution and length (length is based on all selected sequences, resolution is based on just the active sequence)
+
+
+## Demo gif
+ ![alt](/demo_match_sequence.gif)
+
+
+# Blender Show/Hide/Toggle stip modifiers
+This operator quickly alows you to show, hide, or toggle all modifiers of selected strips. See demo below. Note: future goal (but not currently implemented) is to make toggle, or show/hide save "initial state". Meaning: if you select hide, it hides everything, but if you then select show it will only show the modifiers that were initially not muted in the first place.
 
 
 # Demo gif
- ![alt](/demo_button.gif)
+ ![alt](/demo_modifier_toggle.gif)
--- render_match_VSE_strips.py
+++ render_match_VSE_strips.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Render Match VSE Strips",
     "description": "Operator that sets render resolution and start/end frame to match selected VSE strips",
     "author": "Patrick W. Crawford",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (2, 7, 7),
     "location": "VSE strip editor > Properties > Edit Strip (Panel): Match Strips",
     "wiki_url": "",
@@ -53,17 +53,75 @@ class SEQUENCE_OT_match_sequence_resolution(bpy.types.Operator):
 		return {'FINISHED'}
 
 
+class SEQUENCE_OT_show_hide_strip_modifiers(bpy.types.Operator):
+	"""Show, hide, or toggle visibility on all selected strip modifiers"""
+	bl_label = "Match Strip"
+	bl_idname = "sequencer.show_hide_strip_modifiers"
+	bl_description = "Show, hide, or toggle visibility on all selected strip modifiers"
+	bl_options = {'REGISTER', 'UNDO'}
+
+	selection_only = bpy.props.BoolProperty(
+		name="Only Selected",
+		default=True,
+		description="Only apply to selected strips")
+
+	showhide = bpy.props.EnumProperty(
+		items = [('show', 'Show', 'Make modifiers visible'),
+				('hide', 'Hide', 'Make modifiers not visible'),
+				('toggle', 'Toggle', 'Toggle modifier visilibity per modifier')],
+		name = "show/hide",
+		description = "Show, hide, or toggle all strip modifier")
+
+
+	def execute(self, context):
+
+		stps = []
+		if self.selection_only==True:
+			stps = [seq for seq in context.scene.sequence_editor.sequences if seq.select]
+		else:
+			stps = context.scene.sequence_editor.sequences
+
+		for stp in stps:
+			for mod in stp.modifiers:
+				print(stp, mod)
+				if self.showhide=="show":
+					mod.mute=False
+				elif self.showhide=="hide":
+					mod.mute=True
+				else:
+					mod.mute = not mod.mute
+
+			# add arbitrary property to each modifier (or to each stip)
+			# on what the INITIAL state was??
+
+		return {'FINISHED'}
+
+
 def panel_append(self, context):
 	self.layout.operator(SEQUENCE_OT_match_sequence_resolution.bl_idname)
+
+def header_append(self, context):
+	row = self.layout.row(align=True)
+	row.operator(SEQUENCE_OT_show_hide_strip_modifiers.bl_idname,
+		icon="RESTRICT_VIEW_OFF",text="").showhide = "show"
+	row.operator(SEQUENCE_OT_show_hide_strip_modifiers.bl_idname,
+		icon="ARROW_LEFTRIGHT",text="").showhide = "toggle"
+	row.operator(SEQUENCE_OT_show_hide_strip_modifiers.bl_idname,
+		icon="RESTRICT_VIEW_ON",text="").showhide = "hide"
+	
 
 
 def register():
 	bpy.utils.register_class(SEQUENCE_OT_match_sequence_resolution)
+	bpy.utils.register_class(SEQUENCE_OT_show_hide_strip_modifiers)
 	bpy.types.SEQUENCER_PT_edit.append(panel_append)
+	bpy.types.SEQUENCER_HT_header.append(header_append)
 
 def unregister():
 	bpy.utils.unregister_class(SEQUENCE_OT_match_sequence_resolution)
+	bpy.utils.unregister_class(SEQUENCE_OT_show_hide_strip_modifiers)
 	bpy.types.SEQUENCER_PT_edit.remove(panel_append)
+	bpy.types.SEQUENCER_HT_header.append(header_append)
 
 
 if __name__ == "__main__":
